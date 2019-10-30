b10726aac04697be8c9ce7e4cf8f43aee72bc8e1
chen7david <chen7david@me.com>
2019-10-30 08:39:32 +0800

    update

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 const express = require('express')
 const app = express()
-// const bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+var useragent = require('express-useragent')
 const nunjucks = require('nunjucks')
-app.use('/assets', express.static('views/assets'))
+
 nunjucks.configure('views', {
     autoescape: true,
     express: app
 })
 
-// app.use(bodyParser.)
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(useragent.express())
 
+
+app.use('/assets', express.static('views/assets'))
 app.use('/', (req, res) => {
     res.render('index.html', {name:"david"})
+})
+
+app.use((req,res,next)=>{
+    throw({ status: 422, message:'ROUTENOTFOUND'})
+})
+
+app.use((error, req, res, next)=>{
+    console.log(error.message)
+    res.status(200).json({error})
 })
 
 module.exports = app
